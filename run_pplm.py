9e9914db47e7ec191cf46a8b61779b75515a5191
xuchen <chen.xu@geelink.cn>
2020-06-26 20:01:35 +0800

    add transformers

--- run_pplm.py
+++ run_pplm.py
@@ -850,6 +850,7 @@ def run_pplm_example(
     # load pretrained model
     model = GPT2LMHeadModel.from_pretrained(
         pretrained_model,
+        cache_dir='.cache/transformers',
         output_hidden_states=True  # passed to model's __init__ method
     )
     model.to(device)
