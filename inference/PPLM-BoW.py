0f9edd12125ca5584deb0dabc96447f8b5905f03
xuchen <chen.xu@geelink.cn>
2020-10-22 17:49:47 +0800

    PPLM-BoW.py

---
+++ inference/PPLM-BoW.py
@@ -0,0 +1,53 @@
+from run_pplm import run_pplm_example
+
+if __name__ == '__main__':
+    prefixes = [
+        # standard 15 prefixes
+        'Once upon a time',
+        # 'The book', 'The chicken',
+        # 'The city', 'The country',
+        # 'The horse', 'The lake', 'The last time', 'The movie', 'The painting',
+        # 'The pizza', 'The potato', 'The president of the country', 'The road', 'The year is 1910.',
+        # # # extra 35 prefixes
+        # 'The article', 'I would like to', 'We should', 'In the future', 'The cat',
+        # 'The piano', 'The walls', 'The hotel', 'The good news', 'The building',
+        # 'The owner', 'Our house', 'Do you like', 'Her hair', 'The spider man',
+        # 'The computer', 'My phone', 'The TV', 'The bus', 'Long long ago',
+        # 'My daughter', 'The ice cream', 'This recipe', 'Most of us', 'The game',
+        # 'The music', 'The show', 'The dress', 'In the evening', 'The traffic',
+        # 'We usually', 'My mother', 'My dad', 'The meeting', 'My wife',
+    ]
+
+    task = 'positive'
+    sample_methods = 'PPLM-BoW'
+    seed = 20
+    num_samples_each_prefix = 1
+    total_samples = len(prefixes) * num_samples_each_prefix
+    num_iterations = 10
+    step_size = 0.04
+    length = 30
+    verbosity = 'quiet'
+
+    dst_file_name = '{},{},seed={},n={},m={}'.format(
+        task, sample_methods, seed, total_samples, num_iterations)
+
+    dst_file = 'automated_evaluation/generated_samples/{}/{}'.format(task, dst_file_name)
+
+    for prefix in prefixes:
+        with open(dst_file, 'a') as file:
+            run_pplm_example(
+                bag_of_words='dead',
+                cond_text=prefix,
+                num_samples=num_samples_each_prefix,
+                length=length,  # influence random
+                seed=seed,
+                stepsize=step_size,
+                sample=True,
+                num_iterations=num_iterations,
+                gamma=1,
+                gm_scale=0.95,
+                kl_scale=0.01,
+                verbosity=verbosity,
+                file=file,
+                sample_method=sample_methods,
+            )
