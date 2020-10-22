929c3b4c93c2149a9e71568ebd539f5fa2e72e28
xuchen <chen.xu@geelink.cn>
2020-10-22 17:55:41 +0800

    PPLM-BoW.py

--- inference/PPLM-BoW.py
+++ inference/PPLM-BoW.py
@@ -1,6 +1,8 @@
 from run_pplm import run_pplm_example
 
 if __name__ == '__main__':
+
+    # 1.Set prefixes
     prefixes = [
         # standard 15 prefixes
         'Once upon a time',
@@ -18,21 +20,23 @@ if __name__ == '__main__':
         # 'We usually', 'My mother', 'My dad', 'The meeting', 'My wife',
     ]
 
+    # 2.Config Hyperparameters
     task = 'positive'
     sample_methods = 'PPLM-BoW'
     seed = 20
     num_samples_each_prefix = 1
     total_samples = len(prefixes) * num_samples_each_prefix
     num_iterations = 10
-    step_size = 0.04
+    step_size = 0.04  # control strength "dead dead ... dead"
     length = 30
     verbosity = 'quiet'
 
+    # 3.Set Output File Path
     dst_file_name = '{},{},seed={},n={},m={}'.format(
         task, sample_methods, seed, total_samples, num_iterations)
-
     dst_file = 'automated_evaluation/generated_samples/{}/{}'.format(task, dst_file_name)
 
+    # 4.Inference
     for prefix in prefixes:
         with open(dst_file, 'a') as file:
             run_pplm_example(
